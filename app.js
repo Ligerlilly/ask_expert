ef7df138eea614bac23527dfd225bf75c842e871
Jason <quizathaderat@yahoo.com>
2015-09-09 18:31:56 -0700

    Add post route for questions to backend

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + '/public'));
+
+app.use('/', routes);
 
 app.listen(3000, function() {
   console.log('listening on port 3000');
 });
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/questions';
+var pg = require('pg');
+
+router.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../views', 'index.html'));
+});
+
+router.post('/questions', function(req, res) {
+  var results = [];
+
+  var data = { text: req.body.text };
+
+  pg.connect(connectionString, function(err, client, done) {
+    client.query('INSERT INTO queries (text) VALUES ($1)', [data.text]);
+
+    var query = client.query('SELECT * FROM queries ORDER BY id ASC');
+
+    query.on('row', function(row) {
+      results.push(row);
+    });
+
+    query.on('end', function() {
+      client.end();
+      return res.json(results);
+    });
+
+    if(err){
+      console.log(err);
+    }
+  });
+});
+
+
+
+module.exports = router;
