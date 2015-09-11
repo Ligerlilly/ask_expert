708756da1ce4a2dee4c2f2ce83df282334ff21a0
Jason <quizathaderat@yahoo.com>
2015-09-10 22:21:03 -0700

    Clean up front end

--- models/database.js
+++ models/database.js
@@ -1,6 +1,7 @@
+//process.env.DATABASE_URL ||
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/questions';
+var connectionString =  'postgres://localhost:5432/questions';
 var client = new pg.Client(connectionString);
 client.connect();
 var query = client.query('CREATE TABLE queries(id SERIAL PRIMARY KEY, text TEXT NOT NULL)');
-query.on('end', function() { client.end() });
+query.on('end', function() { client.end(); });
--- public/services/AskFactory.js
+++ public/services/AskFactory.js
@@ -10,14 +10,16 @@ askExpert.factory('AskFactory',['$state','$http', function AskFactory($state, $h
 
   factory.questionData = {};
   factory.questionFormData = {};
-  $http.get('/questions')
-    .success(function(data) {
-      factory.questionData = data;
-      console.log(data);
-    })
-    .error(function(error) {
-      console.log('Error: ' + error);
-    });
+  factory.getQuestions = function() {
+    $http.get('/questions')
+      .success(function(data) {
+        factory.questionData = data;
+        console.log(data);
+      })
+      .error(function(error) {
+        console.log('Error: ' + error);
+      });
+  };
   factory.createQuestion = function() {
     $http.post('/questions', factory.questionFormData)
     .success(function(data){
